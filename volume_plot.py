7a5591609d83cc1b8bf2f11af31e4cd02efca957
ItsDezi <juliend290@yahoo.com>
2025-01-25 15:05:35 -0500

    added volume plot using plotly.graph_objects

---
+++ volume_plot.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly.graph_objects as go
+import os
+
+# Function to read CSV and plot two columns
+def plot_csv(file_path1, timestamp, askVolume, bidVolume):
+    # Read the first CSV file and parse the timestamp column as datetime
+    first_csv = [file for file in os.listdir(file_path1) if file.startswith('market_data') and file.endswith('0.csv')][0]
+    #print(first_csv)
+    files = [file for file in os.listdir(file_path1) if file.startswith('market_data') and not file.endswith('0.csv')]
+    #print(files)
+    data1 = pd.read_csv(file_path1 + '/' + first_csv, parse_dates=[timestamp])
+
+    # Rename the columns of the second DataFrame to match the first
+    for file in files:
+        data2 = pd.read_csv(file_path1 + '/' + file, parse_dates=[timestamp], header=None, names=data1.columns)
+        data2.columns = data1.columns
+        data1 = pd.concat([data1, data2], ignore_index=True)
+    
+    # Plot the data using Plotly Graph Objects
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data1[timestamp], y=data1[askVolume], mode='lines', name=askVolume))
+    fig.add_trace(go.Scatter(x=data1[timestamp], y=data1[bidVolume], mode='lines', name=bidVolume))
+    
+    fig.update_layout(title=f'{timestamp} vs {askVolume} and {bidVolume}',
+                      xaxis_title=timestamp,
+                      yaxis_title='Volume')
+    
+    fig.show()
+
+# Example usage
+if __name__ == "__main__":
+    file_path1 = 'data/TrainingData/Period1/C'  # Replace with your CSV file path
+    time = 'timestamp'
+    askVolume = 'askVolume' 
+    bidVolume = 'bidVolume'
+    plot_csv(file_path1, time, askVolume, bidVolume)
